03a4519ecc8923aaaca722f91436f89c4aa7c361
nrufino15 <nrufino@elpuig.xeill.net>
2019-03-21 19:47:40 +0100

    Examples

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,24 +1,70 @@
 package sample;
 
+import com.sun.jdi.LongValue;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 
+    @Override
+    public void start(Stage theStage)
+    {
+        theStage.setTitle( "AnimationTimer Example" );
 
-    public static void main(String[] args) {
-        launch(args);
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        theStage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 512, 512 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Image earth = new Image( "earth.png" );
+        Image sun   = new Image( "sun.png" );
+        Image space = new Image( "space.png" );
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                double x = 232 + 128 * Math.cos(t);
+                double y = 232 + 128 * Math.sin(t);
+
+                // Clear the canvas
+                gc.clearRect(0, 0, 512,512);
+
+                // background image clears canvas
+                gc.drawImage( space, 0, 0 );
+                gc.drawImage( earth, x, y );
+                gc.drawImage( sun, 196, 196 );
+            }
+        }.start();
+
+        theStage.show();
     }
 }
 
