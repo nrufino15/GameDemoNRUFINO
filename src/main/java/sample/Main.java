a65d3b236989f0fb71c79ace5ba2b6817caff232
KennyR <nrufino@elpuig.xeill.net>
2019-04-24 19:19:39 +0200

    Added event OnKeyPressed

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -3,6 +3,11 @@ package sample;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -13,37 +18,40 @@ import javafx.scene.image.Image;
 public class Main extends Application {
 
     @Override
-    public void start(Stage theStage)
-    {
-        theStage.setTitle( "AnimationTimer Example" );
+    public void start(Stage stage) {
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 800, 500);
 
-        Group root = new Group();
-        Scene theScene = new Scene( root );
-        theStage.setScene( theScene );
+        Image space = new Image("/space.png");
+        ImageView scapeImage = new ImageView(space);
+        scapeImage.setFitWidth(800);
+        scapeImage.setFitHeight(500);
+        pane.getChildren().addAll(scapeImage);
 
-        Canvas canvas = new Canvas( 1280, 720 );
-        root.getChildren().add( canvas );
-
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        Image space = new Image("/space.png" );
         Image ship = new Image("/ship.png");
+        ImageView shipImage = new ImageView(ship);
+        shipImage.setFitWidth(120);
+        shipImage.setFitHeight(80);
+        shipImage.setX(pane.getHeight() / 2);
+        shipImage.setY(pane.getWidth() / 2);
+        pane.getChildren().addAll(shipImage);
 
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+            @Override
+            public void handle(KeyEvent event){
 
-        new AnimationTimer() {
-
-            public void handle(long currentNanoTime) {
-                // Clear the canvas
-                gc.clearRect(0, 0, 512,512);
-
-                // background image clears canvas
-                gc.drawImage( space, 0, 0 );
-                gc.drawImage(ship, 0, 0);
+                if (event.getCode() == KeyCode.RIGHT) {
+                    shipImage.setLayoutX(shipImage.getLayoutX() + 15);
+                } else if (event.getCode() == KeyCode.LEFT) {
+                    shipImage.setLayoutX(shipImage.getLayoutX() - 15);
+                }
             }
-        }.start();
+        });
 
-        theStage.show();
-    }
+        stage.setTitle("Asteroids");
+        stage.setScene(scene);
+        stage.show();
+}
 
     public static void main(String[] args) {
         launch(args);
